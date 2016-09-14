066e6b11bfb8967be5641d95397508c9b7df6571
Eoin Gralton <gralton15@gmail.com>
2016-09-14 12:34:13 +0100

    JavaDoc added to Main Class, final system completed as of 14/09/16

--- Healh Insurance Team Project/src/hiPkg/Main.java
+++ Healh Insurance Team Project/src/hiPkg/Main.java
@@ -1,6 +1,7 @@
 package hiPkg;
 
 import java.io.BufferedWriter;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -11,8 +12,21 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
 
+/**
+* Main program to run the system within the console.
+*
+* @author  Gary Mc Monagle, Eoin Gralton, Declan McDaid, Emma Mc Namee, Fiona Bowdren
+* @version 1.0
+* @since   2016-08-01 
+*/
+
 public class Main {
 
+	/** 
+	 * @param user
+	 * @param args
+	 * @throws SQLException
+	 */
 	public static void main(String[] args) throws SQLException {
 		int user = -1;
 		Scanner keyboardIn = new Scanner(System.in);
@@ -29,7 +43,9 @@ public class Main {
 
 			System.out.println("1. New Policy\n2. Admin\n0. Exit");
 			user = keyboardIn.nextInt();
-			
+			/**
+			   * if else to create a menu system, uses email validation to not create the same policy twice
+			   */
 			if(user == 1){
 				boolean uniqueEmail = false;
 				String email = "";
@@ -48,16 +64,26 @@ public class Main {
 				 * 
 				 * 
 				 */
+				/**
+				 * @param sysout
+				   * Policy Types
+				   */
 				System.out.println("Policy Types: GOLD-25% INCREASE PREMIUM CARE +PRIVATE ROOM");
 				System.out.println("Policy Type: SILVER-10% INCREASE PRIVATE ROOM");
 				System.out.println("Policy Type: Bronze-5% INCREASE BASIC POLICY");
-
+				
+				/**
+				 * @param int policyChosen
+				 */
 				int policyChosen = 0; 
 				ArrayList<PolicyType> policyTypes = DatabaseManager.readPolicyTypes();
 				for(int i = 0; i< policyTypes.size(); i++){
 					System.out.println(i+1 + ". "+policyTypes.get(i).getName());
 				}
 				policyChosen = keyboardIn.nextInt();
+				/**
+				   * @ param Client Array List
+				   */
 				ArrayList<Client> policyClients = new ArrayList<Client>();
 				System.out.println("Please enter number of customer for policy");
 				int clientsNeeded = keyboardIn.nextInt();
@@ -68,7 +94,9 @@ public class Main {
 					String lName = keyboardIn.next();
 					System.out.println("Please enter Age");
 					int age = keyboardIn.nextInt();
-					
+					/**
+					   * @param Conditions Array List
+					   */
 					ArrayList<Condition> availCons = DatabaseManager.readConditions();
 					ArrayList<Condition> custCons = new ArrayList<Condition>();
 					int userConditions = -1;
@@ -91,9 +119,14 @@ public class Main {
 				}
 				DatabaseManager.writePolicy(new Policy(policyClients, contactNo, email, policyTypes.get(policyChosen)));
 			
-		
+				/**
+				   * @ param writer
+				   * Write to file section for backup purposes on machine
+				   */
 			Writer writer = null;
-
+			/**
+			 * try catch 
+			 */
 			try {
 				{
 					   Calendar cal = Calendar.getInstance();
@@ -117,8 +150,14 @@ public class Main {
 			/*
 			 * Added 9/09/16
 			 */
+			/**
+			 * @param 
+			   * Call the login screen
+			   */
 			loginScreen.Login.main(args);
-
+			/**
+			   * Admin Section
+			   */
 			if(user == 2){
 				int adminSelect = -1;
 				do{
@@ -158,6 +197,9 @@ public class Main {
 		keyboardIn.close();
 
 	}
+	/**
+	   * Validate Input
+	   */
 	public static boolean validateInput(String input){
 		boolean valid = true;
 		for(int i = 0; i<input.length(); i++){
